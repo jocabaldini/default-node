cfdc895385b0df514adf054dbc4d9f9e9c013dbc
João Baldini <joao.baldini@dextra-sw.com>
2021-07-10 11:06:49 -0300

    Inclusão do dotenv e arquivos de variavéis de ambiente

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const app = express()
 
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env",
+})
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
   res.send("hello world")
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
